ef2512aad632032793c962e625c0b70cae54e185
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-09-28 08:46:28 +0530

    Add Header component

---
+++ Expense-Tracker-App/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>💰 Expense Tracker</h1>
+      <p>Track your income and expenses</p>
+    </header>
+  )
+}
+
+export default Header
