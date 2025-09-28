3e549622894c0a33a3dcbd002b2b0e5cce20c5f0
Bhagya Welivita <bhagyawelivita2021@gmail.com>
2025-09-28 21:40:57 +0530

    Add IncomeExpenses component

--- Expense-Tracker-App/src/App.jsx
+++ Expense-Tracker-App/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import Header from './components/Header/Header'
 import Balance from './components/Balance/Balance'
+import IncomeExpenses from './components/IncomeExpenses/IncomeExpenses'
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Balance transactions={[]} />
+      <IncomeExpenses transactions={[]} />
     </div>
   )
-}
+} 
 
 export default App
